2bb631c258ebc37a394a2daf2afd7705f1987442
MhamedSanaa <mhamed.sanaa6@gmail.com>
2023-03-30 14:56:59 +0100

    oy

--- php/src/index.php
+++ php/src/index.php
@@ -1,34 +1,73 @@
 <?php
-//These are the defined authentication environment in the db service
-
-// The MySQL service named in the docker-compose.yml.
-$host = 'db';
-
-// Database use name
-$user = 'clouduser';
-
-//database user password
-$pass = 'cloudpwd';
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
 
 
-// database name
-$mydatabase = 'clouddb';
-// check the mysql connection status
+$connect = mysqli_connect("db", "clouduser", "cloudpwd");
 
-$conn = new mysqli($host, $user, $pass, $mydatabase);
-
-// select query
-$sql = 'SELECT * FROM users';
-
-if ($result = $conn->query($sql)) {
-    while ($data = $result->fetch_object()) {
-        $users[] = $data;
-    }
+// Check connection
+if (!$connect) {
+  die("Connection failed: " . mysqli_connect_error());
 }
 
-foreach ($users as $user) {
-    echo "<br>";
-    echo $user->username . " " . $user->password;
-    echo "<br>";
+// Create database
+$sql = "CREATE DATABASE clouddb";
+mysqli_query($connect, $sql);
+
+$connection = mysqli_connect("db", "clouduser", "cloudpwd","clouddb");
+
+// Check connection
+if (!$connection) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+// Create table if it does not exist
+$sql = "CREATE TABLE IF NOT EXISTS person (
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  firstname VARCHAR(30) NOT NULL,
+  lastname VARCHAR(30) NOT NULL,
+  password VARCHAR(50) NOT NULL,
+  email VARCHAR(50) NOT NULL
+)";
+
+mysqli_query($connection, $sql);
+
+// Check if the request method is POST
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Get the data from the request body
+    $data = json_decode(file_get_contents("php://input"), true);
+
+    // Validate the data
+    if (isset($data['firstname']) && isset($data['lastname']) && isset($data['email']) && isset($data['password'])) {
+        // Process the data and send a success response
+        $firstname = $data['firstname'];
+        $lastname = $data['lastname'];
+        $email = $data['email'];
+        $password = $data['password'];
+        
+        // Connect to the MySQL database
+        $conn = mysqli_connect("db", "clouduser", "cloudpwd", "clouddb");
+
+        // Insert the data into the database
+        $sql = "INSERT INTO person (firstname, lastname, password, email) VALUES ('$firstname', '$lastname', '$password', '$email')";
+        if (mysqli_query($conn, $sql)) {
+            $response = array("success" => true, "message" => "Data inserted successfully");
+        } else {
+            $response = array("success" => false, "error" => "Error inserting data: " . mysqli_error($conn));
+        }
+
+        // Close the database connection
+        mysqli_close($conn);
+
+        echo json_encode($response);
+    } else {
+        // If the data is invalid, send an error response
+        http_response_code(400);
+        echo json_encode(array("error" => "Invalid data"));
+    }
+} else {
+    // If the request method is not POST, return an error message
+    http_response_code(405);
+    echo json_encode(array("error" => "Method Not Allowed"));
 }
 ?>
